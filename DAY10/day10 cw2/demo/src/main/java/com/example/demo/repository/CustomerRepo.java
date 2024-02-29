3148ec0fd8c01824a88684c588a096d06a1762d4
priyatharshini-10 <124856925+priyatharshini-10@users.noreply.github.com>
2024-02-29 09:06:37 +0530

    Add files via upload

---
+++ DAY10/day10 cw2/demo/src/main/java/com/example/demo/repository/CustomerRepo.java
@@ -0,0 +1,27 @@
+package com.example.demo.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.Customer;
+import java.util.List;
+
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer>{
+
+    @Query("select u from Customer u where u.city=?1")
+    List<Customer> findByCity(String city);
+    
+    @Query("select u from Customer u where u.customerId=?1")
+    Customer findAllCustomerId(int id);
+    
+    @Query("update Customer set pincode=?2 where customerId=?1")
+    Customer updateCustomer(int CustomerID,int pincode);
+   
+    @Query("delete from Customer where customerId=?1")
+    void delete(int CustomerID);
+
+
+}
